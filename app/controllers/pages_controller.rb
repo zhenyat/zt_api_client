80858fd563da2265c8da823da5d975cb4ea27015
Zhenya Telyukov <telyukov@gmail.com>
2017-09-01 20:31:49 +0300

    response handling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,32 @@
 class PagesController < ApplicationController
   def home
-    url = "https://zt-api-server.herokuapp.com/categories/2/subjects"
-    response = RestClient.get(url, headers={})
-    render json: response.body
+    url = "https://zt-api-server.herokuapp.com/categories/1/subjects"
+#    url = "https://btc-e.nz/api/3/info"
+
+    begin
+      response = RestClient.get(url, headers={})
+    rescue RestClient::ExceptionWithResponse => e
+      puts "HTTP Request error: #{e}"
+      render json: e
+    rescue  => e
+      render json: e
+      puts "HTTP Request error: #{e}"
+    else
+      puts "ZT OK! response = #{response}"
+      puts response.to_json.class
+#      render json: response.body
+      render json: handle_response(response.body)
+    end
+  end
+  
+  def handle_response data
+    array = JSON.parse(data)
+    
+    out_data = ''
+    array.each do |el|
+      puts "#{el['name']}: #{el['abstract']}"
+      out_data << "#{el['name']} - #{el['title']}: #{el['abstract']} |  #{el['description']}\n"
+    end
+    out_data
   end
 end
